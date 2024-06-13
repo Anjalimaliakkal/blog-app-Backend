ea405c1f338bf93356f06fab921847ab946cb9c4
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-13 15:21:53 +0530

    blog api completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt = require("bcryptjs")
 const { blogsmodel } = require("./models/blog")
-
+const jwt=require("jsonwebtoken")
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -26,6 +26,34 @@ app.post("/signUp", async(req, res) => {
     let blog=new blogsmodel(input)
     blog.save()
     res.json({ "status": "success" })
+})
+
+app.post("/signIn",(req,res)=>{
+    let input=req.body
+    blogsmodel.find({"emailid":req.body.emailid}).then(
+        (response)=>{
+            if (response.length>0) {
+                let dbPassword=response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+if (isMatch) {
+    jwt.sign({email:input.emailid},"blog-app",{expiresIn:"1d"},(error,token)=>{
+        if (error) {
+            res.json({"status":"unable to create token"})
+        } else {
+            res.json({"status":"success","userid":response[0]._id,"token":token})
+        }
+    })
+    
+} else {
+    res.json({"status":"incorrect password"})
+}
+                })
+            } else {
+                res.json({"status":"user doesn't exist"})
+            }
+        }
+    ).catch()
 })
 
 app.listen(8081, () => {
